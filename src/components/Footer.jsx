1b0309fca9cbdfde6f074051cc7b0a01b0f038d4
DanielMaca30 <daniel.maca@uao.edu.co>
2024-10-03 20:58:00 -0500

    correccion finalizada

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,9 @@ const Footer = () => {
       <div className="social-icons">
         <a href="https://www.instagram.com/daniel.maca_/"><FaInstagram size={30} /></a>
         <a href="https://www.tiktok.com/@daniel.maca?_t=8p6THZe4YNh&_r=1"><FaTiktok size={30} /></a>
-        <a href="https://open.spotify.com/user/19j2tuz5hfg..."><FaSpotify size={30} /></a>
-        <a href="https://github.com/DanielMaca30"><FaGithub size={30} /></a>
+        <a href="https://open.spotify.com/user/19jr1dks22avp7rxnjzsh9tn1?si=yAb8letER-6gplfET8PYEQ"><FaSpotify size={30} /></a>
+        <a href="https://github.com/DanielMaca30?tab=repositories"><FaGithub size={30} /></a>
       </div>
-      <p>&copy; 2024 Daniel Maca. Todos los derechos reservados.</p>
     </footer>
   );
 };
